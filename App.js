fcafca25f07931ad66b381e9967771d4518e33f7
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-16 19:46:14 -0200

    opcoes

--- App.js
+++ App.js
@@ -1,2 +1,13 @@
-const express = require('express')
-const handlebars = require('express-handlebars')
+//carregando modulos 
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    const app = express()
+//Configurações
+
+//Outros
+
+const port = 8080
+app.listen(port, ()=> {
+    console.log('Servidor esta rodando ...')
+})
