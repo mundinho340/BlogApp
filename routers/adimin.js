11a9954ec88d111e229c48a264d57ac903ec0477
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-16 20:15:38 -0200

    Create adimin.js

---
+++ routers/adimin.js
@@ -0,0 +1,16 @@
+ const express = require('express')
+ const router = express.Router()
+
+router.get('/', (req, res)=>{
+    res.send('Página principal do painel ADM')
+})
+
+router.get('/posts', (req, res)=>{
+    res.send('pagina de posts')
+})
+
+router.get('/categorias',(req, res)=>{
+    res.send('Paginas de categorias')
+})
+
+ module.exports = router
