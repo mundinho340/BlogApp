d9f176da0026b8c2866b6d619a2ffd0e33c24015
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-26 19:17:56 -0200

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 //carregando modulos 
     const express = require('express')
-    const handlebars = require('express-handlebars')
-    const bodyParser = require('body-parser')
+    const exphbs  = require('express-handlebars');
     const app = express()
+    const bodyParser = require('body-parser')
+    const handlebars = require('express-handlebars')
     const admin = require('./routers/admin')
     const path = require("path")
+
+    //organize
+    
+//config
+//template Engine
+
+var handle = exphbs.create({
+    defaultLayout: 'main'
+    });
+   
+    //Body Parser
+        app.engine('handlebars', handle.engine);
+
+        app.set('view engine', 'handlebars')
 //Configurações
     //Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
