3e44e61d995630d5de3f73766d8484148dcae314
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-07 16:39:01 -0200

    editar

--- routers/admin.js
+++ routers/admin.js
@@ -59,10 +59,30 @@ router.post("/categorias/nova",(req, res) => {
 })
 
 router.get("/categorias/edit/:id", (req, res)=> {
-    Categoria.findOne({_id: req.body.id}).then((categoria)=>{
+    Categoria.findOne({_id: req.params.id}).then((categoria)=>{
         res.render('admin/editCategorias.handlebars',{categoria: categoria})
 
+    }).catch((erro)=>{
+        req.flash("error_msg", "Esta categoria não existe!")
+        res.redirect("/admin/categorias")
     })
+})
+
+router.post("/categorias/edit", (req, res)=>{
+    Categoria.findOne({_id: req.body.id}).then((categoria)=>{
+        categoria.nome = req.body.nome
+        categoria.slug= req.body.slug
+        
+        categoria.save().then(()=>{
+            req.flash("error_msg", "Categoria editada com sucesso!")
+            req.redirect("/admin/categorias")
+        })
+
+    }).catch((erro)=>{
+        req.flash("error_msg", "Houve um erro ao editar a categoria")
+        res.redirect("/admin/categorias")
+    })
+
 })
 
  module.exports = router
